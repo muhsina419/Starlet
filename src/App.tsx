f04af46ee7d75d8edfada91dcf3d3997d816b922
MUHSINA <muhsinamohammedkutty2003@gmail.com>
2025-07-06 11:07:34 +0530

    accessibilityscore card

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import AccessibilityScore from "./components/AccessibilityScore"
 
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-  
     </Routes>
   )
 }
