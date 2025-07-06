cf820bb32884dd34752615064859e40df26562ac
MUHSINA <muhsinamohammedkutty2003@gmail.com>
2025-07-06 10:50:22 +0530

    Resolve merge conflicts in App.tsx and package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import Home from "./Components/Home";
-
-function App() {
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
@@ -8,17 +5,11 @@ import HomePage from "./pages/HomePage"
 
 const App: React.FC = () => {
   return (
-    <>
-      <Home />
-    </>
-  );
     <Routes>
       <Route path="/" element={<HomePage />} />
   
     </Routes>
   )
 }
-
-export default App;
 
 export default App
