592fabf09a8955bae07b352cd872f6eeddec6552
MUHSINA <muhsinamohammedkutty2003@gmail.com>
2025-07-06 10:40:28 +0530

    complete homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-function App() {
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+
+
+const App: React.FC = () => {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+  
+    </Routes>
   )
 }
 
